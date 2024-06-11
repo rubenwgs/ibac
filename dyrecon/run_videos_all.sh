a139d9971855bc24b1faa959599c854034073fb7
bbalfou <b_balfou@yahoo.com>
2024-06-11 20:31:04 +0200

    added video run script

---
+++ dyrecon/run_videos_all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+
+SEQUENCES=(../DATA_ROOT/video/cat.mp4 skvideo.datasets.bikes)
+MODELS=(relu_base siren_base gaussian_base quadratic_base laplacian_base gaussian_base)
+
+
+for sequence in "${SEQUENCES[@]}"; do
+    for model in "${MODELS[@]}"; do
+
+      # RUN VANILLA MODEL
+      python launch.py                                        \
+          --config ".configs/video/${model}.yaml"             \
+          --train                                             \
+          --predict                                           \
+          dataset.video_path="${sequence}"                    \
+          --exp_dir ../exp_video                              \
+          model.hidden_features=512
+
+      #RUN RESFIELD MODEL
+      python launch.py                                        \
+          --config ".configs/video/${model}.yaml"             \
+          --train                                             \
+          --predict                                           \
+          dataset.video_path="${sequence}"                    \
+          --exp_dir ../exp_video                              \
+          model.hidden_features=512                           \
+          model.resfield_layers=[1,2,3]                       \
+          model.composition_rank=40                           \
+          tag=ResFields
+
+    done
+done
